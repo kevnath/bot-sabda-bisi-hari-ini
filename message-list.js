7c9f7fb7e438e34e9cf217ee980c731c7a2df819
Kevin Nathanael <kevin.nathanaelsoeharjanto@gmail.com>
2019-11-13 16:21:24 +0700

    refactor

---
+++ message-list.js
@@ -0,0 +1,27 @@
+const Message = require('./message')
+
+module.exports = {
+  idleMessages: [
+    new Message('zpz'),
+    new Message('zpz amat ya'),
+    new Message('zepiz'),
+    new Message('zpz bgt'),
+    new Message('MZZMZMZMMZMZMZZMZMZ'),
+    new Message('WOY')
+  ],
+  praiseMessages: [
+    new Message('TERPUJILAH TUHAN'),
+    new Message('PUJI DAN SYUKUR'),
+    new Message('SYUKUR KEPADA BC'),
+    new Message('https://media.discordapp.net/attachments/641081733122490398/641166742483107841/bc_help.jpg', 'attach')
+  ],
+  dailyMessages: [
+    new Message('qerja mzmz anjg'),
+    new Message('qerja woy'),
+  ],
+  tagMessages: [
+    new Message('apa lu ngetag" anjg'),
+    new Message('bcd anjg'),
+    new Message('bcd')
+  ]
+}

496c0d419e11bef5a7df8de2e939cb3ba5b204e0
Kevin Nathanael <kevin.nathanaelsoeharjanto@gmail.com>
2019-11-20 11:28:55 +0700

    fix daily

--- message-list.js
+++ message-list.js
@@ -14,14 +14,14 @@ module.exports = {
     new Message('SYUKUR KEPADA BC'),
     new Message('https://media.discordapp.net/attachments/646276322225553408/646280337067999232/help.jpg', 'attach'),
     new Message('https://media.discordapp.net/attachments/646276322225553408/646278927144189973/redemption.jpg', 'attach'),
-    // new Message('Matius 7:7. "Mintalah, maka akan diberikan kepadamu; carilah, maka kamu akan mendapat; ketoklah, maka pintu akan dibukakan bagimu.'),
-    // new Message('Asevanā ca bālānaṁ, paṇḍitānañca sevanā\nPūjā ca pūjanῑyānaṁ, etammaṅgalamuttamaṁ’ti.\nTak bergaul dengan orang-orang dungu, bergaul dengan para bijaksanawan\nDan menghormat yang patut dihormat, itulah berkah utama. (Maṅgala Sutta)')
+    new Message('Matius 7:7. "Mintalah, maka akan diberikan kepadamu; carilah, maka kamu akan mendapat; ketoklah, maka pintu akan dibukakan bagimu.'),
+    new Message('Asevanā ca bālānaṁ, paṇḍitānañca sevanā\nPūjā ca pūjanῑyānaṁ, etammaṅgalamuttamaṁ’ti.\nTak bergaul dengan orang-orang dungu, bergaul dengan para bijaksanawan\nDan menghormat yang patut dihormat, itulah berkah utama. (Maṅgala Sutta)')
   ],
   dailyMessages: [
     new Message('qerja mzmz anjg'),
     new Message('qerja woy'),
     new Message('https://media.discordapp.net/attachments/646276322225553408/646280337067999232/help.jpg', 'attach'),
-    new Message('https://media.discordapp.net/attachments/646276322225553408/646278589553049621/gaz.jpg', 'attach')
+    new Message('https://media.discordapp.net/attachments/646276322225553408/646300759217930240/gaz_trus.jpg', 'attach')
   ],
   tagMessages: [
     new Message('apa lu ngetag" anjg'),

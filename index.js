1d50d4add8d104610ac573fe943a35e9f4c45551
Kevin Nathanael <kevin.nathanaelsoeharjanto@gmail.com>
2020-05-04 23:19:54 +0700

    migrate to discordjs v12

--- index.js
+++ index.js
@@ -1,64 +1,112 @@
 require('dotenv').config()
 const Discord = require('discord.js'),
-  client = new Discord.Client()
+  discordClient = new Discord.Client(),
+  messageList = require('./message-list'),
+  Message = require('./message')
+
 const redis = require('redis'),
   redisClient = redis.createClient({
     db: process.env.REDIS_DB,
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT
-  })
-const messageList = require('./message-list')
-const Message = require('./message')
-const Promise = require('bluebird')
-Promise.promisifyAll(redisClient)
+  }),
+  { promisify } = require('util'),
+  redisGet = promisify(redisClient.get).bind(redisClient),
+  redisSetEx = promisify(redisClient.setex).bind(redisClient),
+  redisSet = promisify(redisClient.set).bind(redisClient),
+  redisDel = promisify(redisClient.del).bind(redisClient)
 
-client.on('ready', () => {
-  console.log('Sudah siap bersabda')
-  const minutes = 120
-  const diffSecs = minutes * 60 * 1000,
-    startHour = 10,
-    endHour = 21,
-    channel = client.channels.get(process.env.CHANNEL_BCD_ID)
-  setInterval(async () => {
-    const d = new Date(),
-      now = d.getTime(),
-      currHour = d.getHours()
-    if (currHour < startHour || currHour >= endHour) return
+const reminderMins = parseInt(process.env.REMINDER_MINUTES),
+  startHour = parseInt(process.env.START_HOUR),
+  endHour = parseInt(process.env.END_HOUR),
+  reminderSecs = reminderMins * 60 * 100,
+  lastChatKey = 'last_chat_ts'
 
-    const dailyKey = 'daily@' + getDateStr(),
-          cache = await redisClient.getAsync(dailyKey)
-    let answer = null
-    if(cache === null && currHour === startHour) {
-      answer = await getDailyQuote()
+discordClient.on('ready', () => {
+  const d = new Date()
+  console.log('Sudah siap bersabda ' + d.toLocaleString())
+  const channel = discordClient.channels.cache.get(process.env.CHANNEL_BCD_ID)
+  setInterval(() => {
+    sendMessageByInterval(channel)
+  }, 5000)
+});
+
+async function sendMessageByInterval(channel) {
+  const d = new Date(),
+    dailyKey = 'daily@' + d.getFullYear() + (d.getMonth() + 1) + d.getDate(),
+    dailyCache = await redisGet(dailyKey)
+  currHour = d.getHours()
+
+  if (currHour < startHour || currHour >= endHour) return
+
+  let answer = null
+  if (currHour === startHour) {
+    if (dailyCache === null) {
+      answer = pickAnswer(messageList.dailyMessages)
+      await redisSetEx(dailyKey, 3600 * 24, JSON.stringify(answer))
     } else {
-      let lastTs = await redisClient.getAsync('lastTs')
-      if (lastTs !== null) {
-        lastTs = parseInt(lastTs)
-        if (now < lastTs + diffSecs) return
-      }
-      answer = pickAnswer(messageList.idleMessages)
+      answer = JSON.parse(dailyCache)
     }
+<<<<<<< Updated upstream
     send(channel, answer)
     redisClient.set('lastTs', d.getTime())
   }, 5000)
 })
+=======
+  } else {
+    let lastTs = redisGet(lastChatKey)
+    if (lastTs !== null) {
+      lastTs = parseInt(lastTs)
+      if (d.getTime() < lastTs + reminderSecs) return
+    }
+    answer = pickAnswer(messageList.idleMessages)
+  }
+>>>>>>> Stashed changes
 
-client.on('message', async (message) => {
-  if (message.author === client.user) return
+  if (answer !== null) {
+    send(channel, answer)
+  }
+}
 
-  const msgText = message.content.toLowerCase().trim().replace('<@!', '<@') // temporary
-  const zepizMessages = ['zpz', 'zepiz', 'mzz', 'woy', 'woi', 'mzm', 'mzzm']
-  const qerjaMessages = ['kerja', 'qerja']
-  const gamesMessages = ['main', 'mabar', 'maen']
+async function send(channel, message) {
+  try {
+    if (message.type === 'attach') {
+      const attach = new Discord.MessageAttachment(message.content)
+      channel.send(attach)
+    } else {
+      channel.send(message.content)
+    }
+    const d = new Date()
+    await redisSet(lastChatKey, d.getTime())
+  } catch (ex) {
+    console.log('error_sending_message_log', message, ex)
+  }
+}
 
-  const botId = client.user.toString()
-        senderId = message.author.toString()
-  const replyKey = 'reply:' + senderId
-  const replyCtx = await redisClient.getAsync(replyKey);
+discordClient.on('message', (message) => {
+  if (message.author === discordClient.user) return
+  try {
+    replyMessage(message)
+  } catch (ex) {
+    console.log('error_err_internal_log', message, ex)
+  }
+});
+
+async function replyMessage(message) {
+  const channel = message.channel,
+    msgText = message.content.trim().toLowerCase(),
+    zepizMessages = ['zpz', 'zepiz', 'mzz', 'woy', 'woi', 'mzm', 'mzzm'],
+    qerjaMessages = ['kerja', 'qerja'],
+    gamesMessages = ['main', 'mabar', 'maen'],
+    replyKey = 'reply:' + message.author.id,
+    replyCtx = await redisGet(replyKey),
+    botId = `<@!${discordClient.user.id}>`
   let answer = null
+
   if (msgText.includes(botId)) {
-    if(msgText === botId + ' help') {
+    if (msgText === botId + ' help') {
       const answers = []
+<<<<<<< Updated upstream
       send(message.channel, new Message('https://media.discordapp.net/attachments/646276322225553408/646280337067999232/help.jpg', 'attach'))
       answers.push(new Message('Command list:'))
       answers.push(new Message('>>> **Sabda hari ini**:\n1. '+ botId + 
@@ -67,43 +115,63 @@ client.on('message', async (message) => {
       setTimeout(function() {
         answers.forEach(function(ans) {
           send(message.channel, ans)
+=======
+      answers.push(new Message('https://media.discordapp.net/attachments/646276322225553408/646280337067999232/help.jpg', 'attach'))
+      answers.push(new Message('Command list:\n >>> **Sabda hari ini**:\n1. ' + botId + ' sabda\n2. ' +
+        botId + ' berikanlah hambamu arahan\n\n**Puja BC**:\n1. puja ' + botId + '\n2. puja bc'))
+      setTimeout(function () {
+        answers.forEach(function (ans) {
+          send(channel, ans)
+>>>>>>> Stashed changes
         })
-      }, 1500);
+      }, 1000);
       return
-    }
-    if(msgText === botId + ' sabda' || msgText === botId + ' berikanlah hambamu arahan') {
-      answer = await getDailyQuote()
+    } else if(msgText === botId + ' sabda' || msgText === botId + ' berikanlah hambamu arahan') {
+      answer = pickAnswer(messageList.dailyMessages)
     } else if(msgText === 'puja ' + botId) {
       answer = pickAnswer(messageList.praiseMessages)
     } else {
-      answer = pickAnswer(messageList.tagMessages)
-      answer = new Message(answer.content + ' ' + senderId, answer.type)
+      const tmp = pickAnswer(messageList.tagMessages)
+      answer = new Message(tmp.content + ` <@!${message.author.id}>`, tmp.type)
     }
   } else if(replyCtx !== null) {
     if(replyCtx === 'zepiz') {
       if(msgText.includes('zepiz')) {
         answer = pickAnswer(messageList.proudMessages)
+        try {
+          await message.react('💯')
+        } catch(ex) {
+          console.log(ex)
+        }
       } else {
+<<<<<<< Updated upstream
         answer = new Message('rip')
+=======
+        try {
+          await message.react('😢')
+        } catch(ex) {
+          console.log(ex)
+        }
+        answer = new Message('zdz')
+>>>>>>> Stashed changes
       }
     }
-    redisClient.delAsync(replyKey)
+    await redisDel(replyKey)
   } else {
     const pattern = new RegExp('ze*p[^i]z+')
-    if (msgText === 'bc' || msgText === 'bisi' || msgText === 'puja bc') {
+    if (msgText === 'puja bc') {
       answer = pickAnswer(messageList.praiseMessages)
     } else if (msgText === 'rip') {
       answer = new Message('rip')
     } else if (msgText === 'bcd bc') {
-      answer = new Message('bcd bc <@' + process.env.BC_USER_ID + '>')
+      answer = new Message(`bcd bc anjg <@!${process.env.BC_USER_ID}>`)
     } else if (hasWord(msgText, zepizMessages)) {
       const answers = [
         'bcd anjg',
         'qerja gblg',
-        'bcd',
-        'sssssttt'
+        'bcd'
       ]
-      answer = new Message(pickAnswer(answers) + ' ' + senderId)
+      answer = new Message(pickAnswer(answers) + ` <@!${message.author.id}>`)
     } else if (hasWord(msgText, qerjaMessages)) {
       answer = new Message('https://media.discordapp.net/attachments/353098986678386708/639405055061131266/unknown.png', 'attach')
     } else if (hasWord(msgText, gamesMessages)) {
@@ -116,24 +184,13 @@ client.on('message', async (message) => {
         'zepiznya dong',
         'zepiznya jgn lupa mz'
       ]
-      answer = new Message(pickAnswer(answers) + ' ' + senderId)
-      redisClient.setex(replyKey, 60*10, 'zepiz')
+      answer = new Message(pickAnswer(answers) + ` <@!${message.author.id}>`)
+      await redisSetEx(replyKey, 60*10, 'zepiz')
     }
   }
-  if(answer !== null) {
-    setTimeout(function() {
-      send(message.channel, answer)
-    }, 700)
-  }
-  redisClient.set('lastTs', (new Date()).getTime())
-})
-
-function send(channel, message) {
-  if(message.type === 'attach') {
-    const attach = new Discord.Attachment(message.content)
-    channel.send(attach)
-  } else {
-    channel.send(message.content)
+  
+  if (answer !== null) {
+    send(channel, answer)
   }
 }
 
@@ -148,23 +205,4 @@ function pickAnswer(answers) {
   return answers[Math.floor(Math.random() * answers.length)]
 }
 
-function getDateStr(date = new Date()) {
-  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-}
-
-async function getDailyQuote() {
-  const dailyKey = 'daily@' + getDateStr(),
-    cache = await redisClient.getAsync(dailyKey)
-  let answer = null
-  if (cache !== null) {
-    answer = JSON.parse(cache)
-  } else {
-    answer = pickAnswer(messageList.dailyMessages)
-    redisClient.setex(dailyKey, 3600 * 24, JSON.stringify(answer))
-  }
-  if(answer.type === 'text')
-    answer = new Message('**SABDA BC HARI INI**\n' + answer.content, answer.type)
-  return answer
-}
-
-client.login(process.env.DISCORD_TOKEN)
+discordClient.login(process.env.DISCORD_TOKEN)
